1d31c532d07aeeb83bcb5c83c6b765b9ac4fe054
KalbinurK <kkalbinur@yahoo.com>
2020-10-31 12:43:26 -0400

    no treat or trick commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -5,5 +5,12 @@ public class HalloweenTest {
         System.out.println("this is for test");
 
         System.out.println("I'm about to give everyone candy");
+
+        // are we there yet?!
+        // let's play a game
+        // will be not treat and trick
+        // stay home and safe wear mask
+
     }
+
 }
